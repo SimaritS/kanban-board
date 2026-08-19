3d2d64ccb144c11a1caf45fd2bee232558942ecf
SimaritS <121357890+SimaritS@users.noreply.github.com>
2026-08-19 13:36:25 -0700

    Final Project
    Updated files with the final product

---
+++ Assignment.java
@@ -0,0 +1,60 @@
+public class Assignment extends Task{
+    private Date dueDate;
+    private double points;
+    private String text;
+
+    public Assignment(String title, String description, String status, int m, int d, int y, double points){
+        super(title, description, status);
+        this.dueDate = new Date(m,d,y);
+        this.points = points;
+    }
+
+    public Assignment(String title, String description, int m, int d, int y, double points){
+        super(title, description);
+        this.dueDate = new Date(m,d,y);
+        this.points = points;
+    }
+
+    public Assignment(String title, String description, double points){
+        super(title, description);
+        this.dueDate = new Date();
+        this.points = points;
+    }
+
+    public Assignment(String title, String desc, String text, double points) {
+        super(title, desc);
+        this.text = text;
+    }
+
+    public Date getDueDate(){
+        return dueDate;
+    }
+
+    public double getPoints(){
+        return points;
+    }
+
+    public void setDueDate(int m, int d, int y){
+        dueDate.setMonth(m);
+        dueDate.setDay(d);
+        dueDate.setYear(y);
+    }
+
+    public void setPoints(double p){
+        this.points = p;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void displayDetails(){
+        System.out.printf("Title: %s\nDescription: %s\nStatus: %s\nDue Date: %s\nPoints: %s\n",getTitle(), getDescription(), getStatus(), dueDate.displayDate(), points);
+    }
+
+}
